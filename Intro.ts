58e3b0dfd76f952fc75bb26d5246ceee57d6aef8
Sachin Rampur <99388531+sachhi21@users.noreply.github.com>
2022-12-15 21:36:58 +0530

    Update Intro.ts

--- Intro.ts
+++ Intro.ts
@@ -1,12 +1,12 @@
 console.log("hi");
 function greet(person: string, date: Date) {
-  console.log(`hello ${person}, Today is ${date.toDateString()}!`);
+  console.log(`hello ${person}, Today is ${date.toDateString()}!`);  // hello sachin Today is thursday 15 December
 }
 greet("sachin", new Date());
 
 var user = { name: "sachin ", age: 10 };
-console.log(user);
-console.log(user.name);
-console.log(user.age);
-console.log("hello");
-console.log("Hello world");
+console.log(user); //{sachin , 10}
+ console.log(user.name); // sachin
+console.log(user.age); //10
+console.log("hello"); // hello
+console.log("Hello world"); //  Hello world
